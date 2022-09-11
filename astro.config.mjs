7bd29ee6396869bf756e0c23f7364b946ad35493
Alexander Hodes <alexander.hodes@live.com>
2022-09-11 17:20:17 +0200

    added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "http://localhost:3000",
+    integrations: [
+        tailwind(),
+    ]
+});
